b1d124a99ba0d41694ad0d3ceaaf6edfa1f4b48b
Wassilios Lytras <w.lytras@me.com>
2024-09-19 11:33:11 +0200

    Make creation of AS2 message in command atomic and reduce queries on sending by 2.

--- pyas2/tests/test_commands.py
+++ pyas2/tests/test_commands.py
@@ -12,7 +12,8 @@ from pyas2 import settings as app_settings
 from pyas2.models import As2Message, Message, Mdn
 from pyas2.tests import TEST_DIR
 from pyas2.management.commands.sendas2bulk import Command as SendBulkCommand
-
+from django.test.utils import CaptureQueriesContext
+from django.db import connection
 
 @pytest.mark.django_db
 def test_sendbulk_command(mocker, partner, organization):
@@ -79,15 +80,23 @@ def test_sendmessage_command(mocker, organization, partner):
     mocked_delete = mocker.patch(
         "pyas2.management.commands.sendas2message.default_storage.delete"
     )
-    management.call_command(
-        "sendas2message",
-        organization.as2_name,
-        partner.as2_name,
-        test_message,
-        delete=True,
-    )
-    assert mocked_delete.call_count == 1
 
+    with CaptureQueriesContext(connection) as queries:
+
+        management.call_command(
+            "sendas2message",
+            organization.as2_name,
+            partner.as2_name,
+            test_message,
+            delete=True,
+        )
+
+        filtered_queries = [
+            query for query in queries if "SAVEPOINT" not in query["sql"]
+        ]
+
+    assert mocked_delete.call_count == 1
+    assert len(filtered_queries) == 7
 
 @pytest.mark.django_db
 def test_manageserver_command(mocker, organization, partner):
