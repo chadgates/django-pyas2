c3287fa0dd01e435f6ded07423e8a6df9e6b8c8f
Wassilios Lytras <w.lytras@me.com>
2024-09-27 10:45:55 +0200

    Remove transaction commit when sending message.

--- pyas2/management/commands/sendas2message.py
+++ pyas2/management/commands/sendas2message.py
@@ -76,14 +76,6 @@ class Command(BaseCommand):
             status="P",
         )
 
-        # Check if we're inside an atomic block (f.e. in Django Test client). Commit to DB fails
-        # in such a case.
-        if not transaction.get_connection().in_atomic_block:
-            # Store the message before sending. This prevents a "message not found" error when an
-            # async MDN is received from the partner before the sendas2message command as finished
-            # and is commited to DB.
-            transaction.commit()
-
         message.organization = org
         message.partner = partner
         message.send_message(as2message.headers, as2message.content)
